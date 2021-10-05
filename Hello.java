59f75d5a7add14fb8038cef7ff8b8fdcaab2ff0c
gajun755 <48642457+gajun755@users.noreply.github.com>
2021-10-05 17:15:14 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
       public static void main(String [] args){
       
-          System.out.println("this is for myjlc git demo1");
+          System.out.println("this is for myjlc git demo1......");
       }
 
 }
